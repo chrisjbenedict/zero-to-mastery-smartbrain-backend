c01a42b7c73a529a6af9aa7e4ec719c3b0877091
github username <github email address>
2019-04-04 12:30:39 -0400

    working sign in and register

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: "10ed93b516574f44a5596a6b90375860"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
   .increment('entries', 1)
   .returning('entries')
   .then(entries => {
-    res.json(entries)
+    res.json(entries[0]);
   })
-  .catch(err => res.status(400).json('Unable to get entries'))
+  .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage,
+  handleApiCall
 }
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,7 +1,7 @@
 const handleSignin = (req, res, db, bcrypt) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return resp.status(400).json('incorrect form submission')
+    return res.status(400).json('incorrect form submission')
   }
   db.select('email', 'hash').from('login')
   .where('email', '=', email)
@@ -22,5 +22,5 @@ const handleSignin = (req, res, db, bcrypt) => {
 }
 
 module.exports = {
-  handleSignin: handleSignin
+  handleSignin
 }
