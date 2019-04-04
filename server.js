96838c828f123253e20dd509b4594bf90b9f33b5
github username <github email address>
2019-04-04 11:04:57 -0400

    import knex, pg, reorganized into components

--- server.js
+++ server.js
@@ -2,85 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express()
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
 app.use(bodyParser.json())
+
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send(database.users)
-})
+app.get('/', (req, res) => { res.send(database.users) })
 
-app.post('/signin', (req, res) => {
-  var a = JSON.parse(req.body);
-  if (a.username === database.users[0].email && a.password === database.secrets.hash) {
-    res.send('signed in');
-  } else {
-    res.json('access denied');
-  }
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body
-  database.users.push({
-    id: '126',
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length - 1])
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  let found = false;
-  database.users.forEach( user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user)
-    }
-  })
-  if (!found) {
-    res.status(400).json("not found")
-  }
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  let found = false;
-  database.users.forEach( user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++
-      return res.json(user.entries)
-    }
-  })
-  if (!found) {
-    res.status(400).json("not found")
-  }
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
-
-
-app.listen(3000, () => {
-  console.log('app is running on 3000')
-})
+app.listen(3000, () => { console.log('app is running on 3000') })
 
 /*
 /sign in ==> POST success/fail
